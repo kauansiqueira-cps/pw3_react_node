0c7c770bfd5a3842771e1f5faaed961fcc5e45d1
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-09-16 20:38:20 -0300

    Aula1609 - Hooks do Firebase Authentication

--- minideiablog/src/pages/CreatePost/CreatePost.jsx
+++ minideiablog/src/pages/CreatePost/CreatePost.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import style from './CreatePost.module.css';
+// import style from './CreatePost.module.css';
 import { useInsertDocument } from '../../hooks/useInsertDocument';
 import { useAuthValue } from '../../context/AuthContext';
 
