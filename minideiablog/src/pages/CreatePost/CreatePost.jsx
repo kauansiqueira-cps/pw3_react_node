8eddf29fc17ac83de5722a30e87c011d8972953b
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-11-04 20:24:00 -0300

    modified:   minideiablog/src/pages/CreatePost/CreatePost.jsx

--- minideiablog/src/pages/CreatePost/CreatePost.jsx
+++ minideiablog/src/pages/CreatePost/CreatePost.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import banner from "/banner_home.png"
 import style from './CreatePost.module.css';
 import { useInsertDocument } from '../../hooks/useInsertDocument';
-import { useAuthValue } from '../../context/AuthContext';
+// import { useAuthValue } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
