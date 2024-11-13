165c53dfd69aea418a26bfddab4e69e4cdd9e7c7
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-11-13 20:56:18 -0300

    Changes to be committed: 	modified:   minideiablog/src/components/PostDetail.jsx 	modified:   minideiablog/src/pages/Home/Home.jsx

--- minideiablog/src/pages/Home/Home.jsx
+++ minideiablog/src/pages/Home/Home.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import logo from '/MBD.png'
 import styles from './Home.module.css'
-
+import logo from '/MBD.png'
 
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { useState } from 'react'
 
 import PostDetail from '../../components/PostDetail'
 
 const Home = () => {
-
-  const { documents: posts, loading } = useFetchDocuments('posts')
+  const { documents: posts, loading } = useFetchDocuments("posts")
   const navigate = useNavigate()
-
   const [query, setQuery] = useState("")
 
   const handlerSubmit = (e) => {
@@ -24,40 +20,37 @@ const Home = () => {
       return navigate(`/search?q=${query}`)
     }
   }
+
   console.log(loading)
 
   return (
     <>
       <div className={styles.home}>
-        <h1>Veja nossas idéias!</h1>
+        <h1>Veja nossas Idéias!</h1>
         <form className={styles.search_form} onSubmit={handlerSubmit}>
           <input
-            type="text"
+            type='text'
             placeholder='Ou busque por tags...'
-            onChange={(e) => { setQuery(e.target.value) }}
+            onChange={(e) => setQuery(e.target.value)}
           />
           <button className='btn btn-dark'>Pesquisar</button>
         </form>
         <div className='post-list'>
-          {loading && <p>Carregando...</p>}
+          {loading && <p>Carregando... </p>}
           {posts && posts.length === 0 && (
-            <div className={styles.noposts}>
-              <p>Não encontramos Ideias cara!</p>
-              <Link to="/post/create">
-                <img src={logo} alt="Logo do Site" />
-              </Link>
-            </div>
+            <>
+              <div className={styles.noposts}>
+                <p>Não encontramos Iedias cara!</p>
+                <Link to="/post/create">
+                  <img src={logo} alt="Logo do Site" />
+                </Link>
+              </div>
+            </>
           )}
-
-          {
-            posts && posts.map((post) => (
-              <PostDetail key={post.id} post={post} />
-            ))
-          }
+          {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         </div>
       </div>
     </>
-
   )
 }
 
